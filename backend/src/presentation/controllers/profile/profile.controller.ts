9477bf7efe1a2e7afc9070fd3acd98d6cd4a9181
Miguel Pombo <miguel.pomboshape@modec.com>
2025-11-14 17:24:14 -0300

    feat: add documentation in profiles controller

--- backend/src/presentation/controllers/profile/profile.controller.ts
+++ backend/src/presentation/controllers/profile/profile.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, HttpCode } from "@nestjs/common";
+import { ApiOperation, ApiResponse } from "@nestjs/swagger";
 import { ProfilesService } from "src/application/profiles/services/profiles.service";
 import { Profile } from "src/core/profiles/entities/profile.entity";
 
@@ -8,6 +9,9 @@ export class ProfileController {
 
   @Get()
   @HttpCode(200)
+  @ApiOperation({ summary: "Retrieve a list of registered profiles" })
+  @ApiResponse({ status: 200, description: "List of profiles" })
+  @ApiResponse({ status: 400, description: "Bad request" })
   async findAllProfiles(): Promise<Profile[]> {
     return await this._profilesService.findAllProfilesAsync();
   }
