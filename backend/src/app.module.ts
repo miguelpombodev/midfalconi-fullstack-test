b3a8d5ad27467eec4727e76b51c6d95fad692588
Miguel Pombo <miguel.pomboshape@modec.com>
2025-11-14 16:59:30 -0300

    feat: add structured logging with Pino

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,10 +5,27 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { InfrastructureModule } from "./infrastructure/infrastructure.module";
 import { PresentationModule } from "./presentation/presentation.module";
 import { ApplicationModule } from "./application/application.module";
+import { LoggerModule } from "nestjs-pino";
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: ".env" }),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        level: process.env.NODE_ENV !== "production" ? "debug" : "info",
+
+        transport:
+          process.env.NODE_ENV !== "production"
+            ? {
+                target: "pino-pretty",
+                options: {
+                  colorize: true,
+                  singleLine: true,
+                },
+              }
+            : undefined,
+      },
+    }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { Logger } from "nestjs-pino";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  app.useLogger(app.get(Logger));
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle("API Documentation")
@@ -11,12 +18,11 @@ async function bootstrap() {
     .setVersion("1.0")
     .build();
 
-  const documentFactory = () =>
-    SwaggerModule.createDocument(app, config, {
-      autoTagControllers: true,
-    });
+  const documentFactory = SwaggerModule.createDocument(app, config, {
+    autoTagControllers: true,
+  });
 
-  SwaggerModule.setup("docs", app, documentFactory());
+  SwaggerModule.setup("docs", app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
