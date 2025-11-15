f9578ad9901fb0e26c8f5e9f39bf713ca91796c6
Miguel Pombo <miguel.pomboshape@modec.com>
2025-11-15 05:01:21 -0300

    fix: fix seeder duplicating items

--- backend/src/infrastructure/persistence/seeds/01-CreateProfiles.seeder.ts
+++ backend/src/infrastructure/persistence/seeds/01-CreateProfiles.seeder.ts
@@ -1,14 +1,23 @@
+import { getConnection } from "typeorm";
 import { Profile } from "../../../core/profiles/entities/profile.entity";
 import { Factory, Seeder } from "typeorm-seeding";
 
 export default class CreateProfilesSeeder implements Seeder {
   public async run(factory: Factory): Promise<void> {
-    await factory(Profile)().create({
-      name: "Administrator",
-    });
+    const profileRepository = getConnection().getRepository(Profile);
+    const profilesToCreate = [{ name: "Administrator" }, { name: "User" }];
 
-    await factory(Profile)().create({
-      name: "User",
-    });
+    for (const profileData of profilesToCreate) {
+      const profileName = profileData.name;
+
+      const existingProfile = await profileRepository.findOne({
+        where: { name: profileName },
+      });
+
+      if (!existingProfile) {
+        console.log(`Creating profile: ${profileName}`);
+        await factory(Profile)().create(profileData);
+      }
+    }
   }
 }
